a1a3680e53815b6ea402866fde3afbd53e08ac26
gitalf96 <126389189+gitalf96@users.noreply.github.com>
2024-06-12 14:29:03 +0530

    Add files via upload

--- Pharma_Sales.py
+++ Pharma_Sales.py
@@ -1,18 +1,14 @@
-import sys
-print(sys.executable)
 import snowflake
 import snowflake.connector
 import pandas as pd
-import warnings
-warnings.filterwarnings("ignore")
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import streamlit as st
 from streamlit_option_menu import option_menu 
-import streamlit_dynamic_filters # type: ignore
-from streamlit_dynamic_filters import dynamic_filters # type: ignore
-import altair 
+# import streamlit_dynamic_filters # type: ignore
+# from streamlit_dynamic_filters import dynamic_filters # type: ignore
+# import altair 
 
 
 
